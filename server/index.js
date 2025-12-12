5b1e8239ec2059c70378a44db15fce548eed18d1
HBr <198370047@qq.com>
2025-12-12 15:58:02 +0800

    express框架和数据库连接

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/backstagemanagement')
+.then(()=>{
+    console.log("connected to database");
+})
+.catch((err)=>{
+    console.log('connected error:',err);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
