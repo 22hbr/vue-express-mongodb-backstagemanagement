05f6667542060ae0225388f2aab6faded51ea51e
HBr <198370047@qq.com>
2025-12-18 23:01:42 +0800

    token过期处理

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { ElLoading,ElMessage } from 'element-plus';
+import { ElLoading, ElMessage } from 'element-plus';
+import router from '@/router';
 
 // 设置加载动画
 let loading;
@@ -20,6 +21,12 @@ const endLoading = () => {
 axios.interceptors.request.use((config => {
   // 开始加载动画
   startLoading();
+
+  if (localStorage.getItem('token')) {
+    // 设置统一的请求头
+    config.headers.Authorization = localStorage.getItem('token');
+  }
+
   return config;
 }), (error => {
   return Promise.reject(error);
@@ -34,6 +41,17 @@ axios.interceptors.response.use((response => {
   // 结束加载动画
   endLoading();
   ElMessage.error(error.response.data);
+
+  const { status } = error.response;
+  if (status === 401) {
+    ElMessage.error('身份验证失败，请重新登录');
+    // 清除本地token
+    localStorage.removeItem('token');
+    // 跳转到登录页面
+    // window.location.href = '/login';
+    router.push('/login');
+  };
+
   return Promise.reject(error);
 }));
 
